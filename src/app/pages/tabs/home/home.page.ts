36c467568601deda0fa4f0fb303ea640ac8231d5
Vinicius Campos <viniolic2018@gmail.com>
2024-06-28 16:21:50 -0300

    editing home page

--- src/app/pages/tabs/home/home.page.ts
+++ src/app/pages/tabs/home/home.page.ts
@@ -1,6 +1,5 @@
 import { Component, ElementRef } from '@angular/core';
 import { SwiperContainer } from 'swiper/element';
-import { SwiperOptions } from 'swiper/types';
 
 interface IAccount {
     id: number;
@@ -8,22 +7,47 @@ interface IAccount {
     balance: string;
 }
 
+interface IFeature {
+    id: number;
+    color: string;
+    icon: string;
+    name: string;
+}
+
+interface ITransactions {
+    id: number;
+    to: string;
+    date: string;
+    amount: number;
+}
+
 @Component({
     selector: 'app-home',
     templateUrl: './home.page.html',
     styleUrls: ['./home.page.scss'],
 })
-export class HomePage  {
-
-    config: SwiperOptions = {
-        navigation: true,
-    };
-
+export class HomePage {
     accounts: IAccount[] = [
         { id: 1, acc_no: '578694509', balance: '200000' },
         { id: 2, acc_no: '220552780', balance: '50000' },
         { id: 3, acc_no: '401630812', balance: '80000' },
     ];
+
+    features: IFeature[] = [
+        { id: 1, color: 'tertiary', icon: 'paper-plane', name: 'Send' },
+        { id: 2, color: 'white', icon: 'send', name: 'Request' },
+        { id: 3, color: 'success', icon: 'add-circle', name: 'Top-up' },
+        { id: 4, color: 'light', icon: 'newspaper', name: 'Bills' },
+        { id: 5, color: 'warning', icon: 'card', name: 'Cards' },
+    ];
+
+    transactions: ITransactions[] = [
+        { id: 1, to: 'Piyush Ag.', date: '2022-05-22', amount: 5000 },
+        { id: 2, to: 'Avinash', date: '2022-03-02', amount: 7000 },
+        { id: 3, to: 'Catherine', date: '2022-07-28', amount: -3250 },
+        { id: 4, to: 'Akhil Ag.', date: '2022-01-09', amount: 1000 },
+        { id: 5, to: 'Prem Ag.', date: '2022-04-13', amount: -800 },
+      ];
 
     index = 0;
 
